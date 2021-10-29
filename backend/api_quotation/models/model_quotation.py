0d7dde5a56ed26bd72cc6184c2286dbaf33e078d
katgzco <katgzco@gmail.com>
2021-10-29 10:02:09 -0500

    Feature: add max_time var to time_estimator method and return dic of post view quotation

--- backend/api_quotation/models/model_quotation.py
+++ backend/api_quotation/models/model_quotation.py
@@ -45,12 +45,15 @@ class Quotation(BaseModel):
         cm2 = self.size
         if cm2 <= 7:
             estimated_time = '1 hour'
+            max_time = '1'
         elif cm2 <= 15:
             estimated_time = 'Between 3 to 4 hours'
+            max_time = '4'
         else:
             estimated_time = 'Between 5 to 6 hours'
+            max_time = '6'
 
-        return str(estimated_time)
+        return [estimated_time, max_time]
 
     def price_estimator(self, artist_id):
         cm2 = self.size
