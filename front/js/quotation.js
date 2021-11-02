9ca1be87f53250b71ba8eaa2b9782def0c4297ec
Vanessa <vagava.123@gmail.com>
2021-11-02 02:16:02 -0500

    scheduling section: up to here you can list the available hours for a given date. I have a bug, because you can select more than one hour.

--- front/js/quotation.js
+++ front/js/quotation.js
@@ -9,18 +9,18 @@ let selectBody = document.getElementById("selectBody");
 let selectSize = document.getElementById("selectSize");
 let quotationImage = document.getElementById("quotationImage");
 let quotationDescription = document.getElementById("quotationDescription");
+let responseQuotation = {}
 const makeQuote = document.getElementById("makeQuote");
 const secctionQuoteResult = document.getElementById("secctionQuoteResult");
+const checkAvailability = document.getElementById("checkAvailability")
 
 //Obtener la lista de los artistas
 async function getDataArtist() {
-  let url = "http://54.233.163.4/api/v1/artists/"
+  let url = "http://18.230.82.177/api/v1/artists/"
   try {
     const respuesta = await fetch(url);
-    console.log(respuesta.status);
     if (respuesta.status === 200) {
       listArtist = await respuesta.json();
-      console.log(listArtist);
       renderArtist();
     }
   } catch (error) {
@@ -30,17 +30,15 @@ async function getDataArtist() {
 
 // seleccionar  y renderizar la informacion del artista
 async function renderArtist() {
-  let url = `http://54.233.163.4/api/v1/artists/${listArtist[0].id}`;
+  let url = `http://18.230.82.177/api/v1/artists/${listArtist[0].id}`;
   try {
     let response = await fetch(url);
     if (response.status === 200) {
       artist = await response.json();
-      console.log(artist);
     }
   } catch (error) {
     console.log(error);
   }
-  console.log('El artista es: ', artist)
   const artistInfo = `
   <div class="row">
     <div class="col-12 col-md-6 mb-1">
@@ -102,7 +100,7 @@ async function sendDataQuotation(event) {
     }
   }
 
-  const url = "http://54.233.163.4/api/v1/quotation/create";
+  const url = "http://18.230.82.177/api/v1/quotation/create";
   let response = await fetch(url, {
     "method": "POST",
     "headers": {
@@ -110,45 +108,122 @@ async function sendDataQuotation(event) {
     },
     "body": JSON.stringify(dataQuotation)
   })
-    .then((response) => response.text()) // respuesta no se puede leer, se tranforma en json
+    .then((response) => response.json()) // respuesta no se puede leer, se tranforma en json
     .then((respuestaJson) => {
-      console.log("respuesta", respuestaJson);
+      responseQuotation = respuestaJson;
       renderQuotation(respuestaJson);
     })
     .catch((razon) => {
       console.log("enviar datos fallo, razon:", razon);
     });
-  console.log(dataQuotation);
 }
 
 function renderQuotation(dataQuotation) {
-  let responseQuotation = JSON.parse(dataQuotation);
+  //let responseQuotation = JSON.parse(dataQuotation);
   const quotationInfo = `
-  <h6 class="mb-4 text-center fs-1">Quote Result</h6>
+  <h6 class="mb-4 text-center fs-1" focus>Quote Result</h6>
   <div class="mb-3 px-3" id="quoteResult">
     <div class="row py-3">
       <div class="col-12 col-md-6 mb-1">
-        <label for="estimatedTime" class="pb-0 fs-4">Estimated time</label>
-        <input type="text" id="estimatedTime" class="form-control form-control-sm fs-5" value="${responseQuotation.time}" placeholder="Estimated time" aria-label="Estimated time" readonly />
+        <label for="estimatedTime" class="pb-0 fs-6 fw-bold">Estimated time</label>
+        <input type="text" id="estimatedTime" class="form-control form-control-sm fs-6" value="${responseQuotation.time}" placeholder="Estimated time" aria-label="Estimated time" readonly />
       </div>
       <div class="col-12 col-md-6 mb-1">
-        <label for="estimatedPrice" class="pb-0 fs-4">Estimated price</label>
-        <input type="text" id="estimatedPrice" class="form-control form-control-sm fs-5" value="${responseQuotation.price}" placeholder="Estimated price" aria-label="Estimated price" readonly />
+        <label for="estimatedPrice" class="pb-0 fs-6 fw-bold">Estimated price</label>
+        <input type="text" id="estimatedPrice" class="form-control form-control-sm fs-6" value="${responseQuotation.price}" placeholder="Estimated price" aria-label="Estimated price" readonly />
       </div>
       <small id="emailHelp" class="form-text text-muted ">This is a price and an approximate time of the session of your tattoo, the artist will decide the final price, which can vary between <spam class="fw-bold">+50,000 COP and -50,000 COP</spam></small>
     </div>
   </div>`;
   secctionQuoteResult.innerHTML = quotationInfo;
+  renderAgenda();
+  window.scrollTo(0, document.body.scrollHeight);
+}
+
+/*Agenda */
+function renderAgenda() {
+  const agendaInfo = `
+  <h6 class="mb-4 text-center fs-1">Agenda</h6>
+  <div class="mb-3 px-3">
+    <div class="row" >
+      <div class="col mb-2">
+        <label for="chooseDate" class="pb-0">Select a date</label>
+        <input type="date" class="form-control" id="chooseDate"pattern="3.+" minlength="10" maxlength="10" placeholder="3998887744" required>
+      </div>
+      <div class="col mb-2 d-grid gap-1 pb-3 pt-3">
+        <button type="submit" onclick="sendDateAgenda()" class="btn btn-lg btn-rescot" id="checkAvailability">Check Availability</button>
+      </div>
+    <div class="row">
+      <div class="col-12 col-sm-8 offset-sm-2 mb-2 d-grid gap-2" id="availability">
+        <!-- availability  -->
+      </div>
+    </div>
+    </div>
+  `;
+  const secctionAgenda = document.getElementById("secctionAgenda");
+  secctionAgenda.innerHTML = agendaInfo;
+
+}
+
+async function sendDateAgenda(event) {
+  //event.preventDefault();
+  const urlAvailability = "http://18.230.82.177/api/v1/calendar/availability"
+  let [year, month, day] = getDateAgenda();
+  const dataAvailability = {
+    "year": year,
+    "month": month,
+    "day": day,
+    "total_time": responseQuotation.max_time,
+    "quotation_id": responseQuotation.id_quotation
+  }
+  const responseAvailability = await fetch(urlAvailability, {
+    "method": "POST",
+    "headers": {
+      "Content-Type": "application/json",
+    },
+    "body": JSON.stringify(dataAvailability)
+  })
+    .then((response) => response.json())
+    .catch((razon) => {
+      console.log("enviar datos 'dataAvailability' fallo, razon:", razon);
+    });
+  renderAvailability(responseAvailability);
+}
+
+function getDateAgenda() {
+  let date = document.getElementById("chooseDate")
+  return date.value.split("-")
+}
+
+function renderAvailability(obj) {
+  console.log(obj)
+  let availabilityInfo = obj.map((hour, indice) => `
+  <button type="submit" class="form-check p-1 btn btn-lg btn-rescot2 m-2 hour-cls" onclick="sendDateHour()">
+    <input class="form-check-input" type="radio" name="${hour}" id="${hour}"
+    value="${hour}">
+    <label class="form-check-label fs-6" for="${hour}">
+    ${hour}
+    </label>
+  </button>`).join("");
+  const availability = document.getElementById("availability");
+  availability.innerHTML = availabilityInfo;
+}
+
+function sendDateHour() {
+  let listHours = document.getElementsByClassName("hour-cls")
+  arrayHours = Array.from(listHours)
+  console.log(arrayHours)
+  /*arrayHours.filter(function (value, index, array) {
+    if (value.children[0].checked === true) {
+      console.log(value.children[0].checked);
+      console.log(value);
+      return value;
+    }
+  })
+
+  //listHours[1].children[0].checked)*/
+
 }
 
 getDataArtist();
 makeQuote.onclick = sendDataQuotation;
-/*
-[x] Obtener la lista de los artistas
-[x] seleccionar  y renderizar la informacion del artista
-[x] Obtener los datos del usuario y de la cotizacion desde el formulario
-[x] enviar la informacion
-[] mostrar el resultado de la cotizacion
-
-
-*/
